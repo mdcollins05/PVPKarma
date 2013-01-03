aab65aee226570f250348af9cdb8847b76f2816e
MDCollins <matt@m-collins.com>
2013-01-03 06:27:42 -0500

    Added project file and some basic code

--- src/main/java/com/blockmovers/plugins/pvpkarma/PVPKarma.java
+++ src/main/java/com/blockmovers/plugins/pvpkarma/PVPKarma.java
@@ -1,22 +1,36 @@
 package com.blockmovers.plugins.pvpkarma;
 
+import java.util.logging.Logger;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.kitteh.tag.PlayerReceiveNameTagEvent;
 
 public class PVPKarma extends JavaPlugin implements Listener {
-    public void onDisable() {
-        // TODO: Place any custom disable code here.
+    
+    static final Logger log = Logger.getLogger("Minecraft"); //set up our logger
+    
+    public void onEnable() {
+        PluginDescriptionFile pdffile = this.getDescription();
+        PluginManager pm = this.getServer().getPluginManager(); //the plugin object which allows us to add listeners later on
+
+        pm.registerEvents(this, this);
+
+        log.info(pdffile.getName() + " version " + pdffile.getVersion() + " is enabled.");
     }
 
-    public void onEnable() {
-        getServer().getPluginManager().registerEvents(this, this);
+    public void onDisable() {
+        PluginDescriptionFile pdffile = this.getDescription();
+
+        log.info(pdffile.getName() + " version " + pdffile.getVersion() + " is disabled.");
     }
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        event.getPlayer().sendMessage("Welcome, " + event.getPlayer().getDisplayName() + "!");
+    public void onNameTag(PlayerReceiveNameTagEvent event) {
+        //event.getPlayer() The player who will see the tag
+        //event.getNamedPlayer() The player who will have the modified tag
     }
 }
 
