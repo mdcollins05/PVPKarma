f273c3d73a8999c74b186c0f2a8f17d872ac9885
MDCollins <matt@m-collins.com>
2013-01-06 17:49:20 -0500

    Added commands and got everything working. At least I think it's all working.

---
+++ src/main/java/com/blockmovers/plugins/pvpkarma/Karma.java
@@ -0,0 +1,90 @@
+package com.blockmovers.plugins.pvpkarma;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Karma {
+
+    PVPKarma plugin = null;
+    FileConfiguration karma = null;
+    private File karmaFile = null;
+    private Map<String, Boolean> PVPToggle = new HashMap(); 
+
+    public Karma(PVPKarma plugin) {
+        this.plugin = plugin;
+    }
+    
+    public void reload() {
+        if (karmaFile == null) {
+            karmaFile = new File(plugin.getDataFolder(), "karma.yml");
+        }
+        karma = YamlConfiguration.loadConfiguration(karmaFile);
+
+        // Look for defaults in the jar
+        InputStream defConfigStream = plugin.getResource("karma.yml");
+        if (defConfigStream != null) {
+            YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
+            karma.setDefaults(defConfig);
+        }
+    }
+
+    public FileConfiguration load() {
+        if (karma == null) {
+            reload();
+        }
+        return karma;
+    }
+
+    public void save() {
+        if (karma == null || karmaFile == null) {
+            return;
+        }
+        try {
+            karma.save(karmaFile);
+        } catch (IOException ex) {
+            Logger.getLogger(JavaPlugin.class.getName()).log(Level.SEVERE, "Could not save the file: " + karmaFile, ex);
+        }
+    }
+    
+    public int getKarma(String p) {
+        return this.karma.getInt("karma." + p, 0);
+    }
+    
+    public void setKarma(String p, int k) {
+        if (k == 0) {
+            this.karma.set("karma." + p, null);
+        } else {
+            this.karma.set("karma." + p, k);
+        }
+        this.save();
+    }
+    
+    public boolean getPVP(String p) {
+        if (this.PVPToggle.containsKey(p)) {
+            return this.PVPToggle.get(p);
+        }
+        return false;
+    }
+    
+    public boolean togglePVP(String p) {
+        if (this.getPVP(p)) {
+            this.PVPToggle.remove(p);
+            this.karma.set("PVPToggle." + p, null);
+            this.save();
+            return false;
+        } else {
+            this.PVPToggle.put(p, true);
+            this.karma.set("PVPToggle." + p, true);
+            this.save();
+            return true;
+        }
+    }
+}
